ae9a785562dfb8b5285c590e74e97323808bbeea
stefanosEshare <stefanos.athanasoulas@ncryptedcloud.com>
2021-02-21 01:00:33 +0200

    Final changes

--- src/utils/getInitialListState.ts
+++ src/utils/getInitialListState.ts
@@ -1,6 +1,11 @@
 import { Movie } from "../api/types/Movie";
 
 export const getInitialListState = (list: Movie[]) => {
+
+    if(list.length === 0) {
+        return []
+    }
+
     return list.map((movie: Movie) => {
         return {
             ...movie,
